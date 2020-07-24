a975d5836565eaabc339f2fdcd5033702df41055
Administrator <yuy20170301@126.com>
2020-07-24 20:29:03 +0800

    '用户添加div警告'

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from "vue"
 import Button from './Button'
 import Icon from "./Icon";
+import Buttongroup from './Button-group'
 Vue.component('g-button', Button)
 Vue.component('icon',Icon)
+Vue.component('button-group',Buttongroup)
 new Vue({
   el:'#app',
 data:{
