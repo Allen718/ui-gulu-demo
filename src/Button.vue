9b14712f45d996c06c6b69a3aad4324b9422b97f
Administrator <yuy20170301@126.com>
2020-07-24 13:31:10 +0800

    '创建name与iconPosition属性'

--- src/Button.vue
+++ src/Button.vue
@@ -1,9 +1,16 @@
 <template>
-  <button class="g-button">hi</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if='name' class="icon" aria-hidden="true">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+    <div class="content">
+      <slot/>
+    </div>
+  </button>
 </template>
 <script>
   export default {
-
+props:['name','iconPosition']
   }
 </script>
 <style lang="scss">
@@ -14,6 +21,36 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    >.content{
+      order:2
+    }
+    >.icon{
+        width: 1em;
+        height: 1em;
+        fill: currentColor;
+        overflow: hidden;
+      order:1;
+margin-right: 0.3em;
+    }
+    &.icon-right{
+      >.content{
+        order:1
+      }
+      >.icon{
+        width: 1em;
+        height: 1em;
+        fill: currentColor;
+        overflow: hidden;
+        order:2;
+        margin-right: 0;
+        margin-left: 0.3em;
+
+      }
+    }
     &:hover {
       border-color: var(--border-color-hover);
     }
