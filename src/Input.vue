6abc82030ef46219ffae1a1978932d3b4d658611
Administrator <yuy20170301@126.com>
2020-07-26 16:57:26 +0800

    添加input.vue及其样式

---
+++ src/Input.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly"/>
+    <template v-if="error">
+      <icon class="icon-error" name="error"></icon>
+      <span>{{error}}</span>
+    </template>
+  </div>
+</template>
+<script>
+  export default {
+    name: "Input",
+    props: {
+      value: {
+        type: String
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      readonly: {
+        type: Boolean,
+        default: false,
+
+      },
+      error: {
+        type: String,
+      }
+
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $border-color: #999;
+  $height: 32px;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 14px;
+  $box-shadow-color: rgba(0, 0, 0, 0.5);
+  $red: #F1453D;
+  .wrapper {
+    display: inline-block;align-items: center;justify-content: center;
+
+    > input {
+      height: $height;
+      border: 1px solid $border-color;
+      padding: 0 8px;
+      border-radius: $border-radius;
+      font-size: $font-size;
+
+      &:hover {
+        border-color: $border-color-hover;
+      }
+
+      &:focus {
+        box-shadow: inset 0 1px 2px $box-shadow-color;
+        outline: none;
+      }
+
+      &[disabled] {
+        border-color: #aaa;
+        color: #aaa;
+        cursor: not-allowed;
+      }
+
+      &[readonly] {
+        border-color: #aaa;
+        color: #aaa;
+
+      }
+    }
+
+    &.error {
+      > input {
+        border-color: $red;
+      }
+
+      > .icon-error {
+        fill: $red
+      }
+
+      > span {
+        color: $red;
+      }
+    }
+  }
+
+</style>
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import Vue from "vue"
-import Button from './Button'
+import Vue from "vue";
+import Button from './Button';
 import Icon from "./Icon";
-import Buttongroup from './Button-group'
+import Buttongroup from './Button-group';
+import Input from './Input'
+Vue.component('g-input',Input)
 Vue.component('g-button', Button)
 Vue.component('icon',Icon)
 Vue.component('button-group',Buttongroup)
@@ -13,84 +15,3 @@ data:{
   loading3:false
 }
 })
-//测试用例
-// import chai from 'chai'
-// import spies from 'chai-spies'
-// chai.use(spies)
-// const expect = chai.expect
-// {
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData: {
-//       icon: 'settings'
-//     }
-//   })
-//   vm.$mount()
-//   let useElement = vm.$el.querySelector('use')
-//   let href = useElement.getAttribute('xlink:href')
-//   expect(href).to.eq('#i-settings')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData: {
-//       icon: 'settings',
-//       loading:true
-//     }
-//   })
-//   vm.$mount()
-//   let useElement = vm.$el.querySelector('use')
-//   let href = useElement.getAttribute('xlink:href')
-//   expect(href).to.eq('#i-load-5')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-// const div = document.createElement('div')
-// document.body.appendChild(div)
-// const Constructor = Vue.extend(Button)
-// const vm = new Constructor({
-//   propsData: {
-//     icon: 'settings'
-//   }
-// })
-// vm.$mount(div)
-// let svg = vm.$el.querySelector('svg')
-// let {order} = window.getComputedStyle(svg)
-// expect(order).to.eq('1')
-// vm.$el.remove()
-// vm.$destroy()
-// }
-// {
-//   const div = document.createElement('div')
-//   document.body.appendChild(div)
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData: {
-//       icon: 'settings',
-//       iconPosition: 'right'
-//     }
-//   })
-//   vm.$mount(div)
-//   let svg = vm.$el.querySelector('svg')
-//   let {order} = window.getComputedStyle(svg)
-//   expect(order).to.eq('2')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData: {
-//       icon: 'settings',
-//     }
-//   })
-//   vm.$mount()
-//   let spy = chai.spy(function(){})
-//   vm.$on('click', spy)
-//   let button = vm.$el
-//   button.click()
-//   expect(spy).to.have.been.called()
-// }
