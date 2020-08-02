77d80311c6805abfc23d9a3130ce3273c216a648
Administrator <yuy20170301@126.com>
2020-08-02 15:39:47 +0800

    添加4个方向

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="popover" @click="Click" ref="popover">
-    <div v-if=" visible" class="content-wrapper" ref="contentWrapper">
+    <div v-if=" visible" class="content-wrapper" ref="contentWrapper" :class="{[ `position-${this.position}`]:true}">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -19,13 +19,34 @@
         visible: false
       }
     },
+    props:{
+      position:{
+        type:String,
+        default:'top',
+        validator(value){
+         return  ['top','bottom','left','right'].indexOf(value)>=0
+        }
+
+      }
+  },
     methods: {
       positionContent() {
           document.body.appendChild(this.$refs.contentWrapper)
-          let { left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
-          this.$refs.contentWrapper.style.left = left + window.scrollX + "px"
-          this.$refs.contentWrapper.style.top = top + window.scrollY + "px"
-
+        const{contentWrapper,triggerWrapper}=this.$refs
+        let { width,left, top,bottom,height} = triggerWrapper.getBoundingClientRect()
+        let height1=contentWrapper.getBoundingClientRect().height
+        contentWrapper.style.left = left + window.scrollX + "px"
+        if(this.position==='top'){
+         contentWrapper.style.top = top+ window.scrollY + "px"
+        }else if(this.position==='bottom'){
+          contentWrapper.style.top = bottom+ window.scrollY + "px"
+        }else if(this.position==='left'){
+          contentWrapper.style.top = top-(height1-height)/2+ window.scrollY + "px"
+          contentWrapper.style.left = left + window.scrollX + "px"
+        }else if(this.position==='right'){
+          contentWrapper.style.top = top-(height1-height)/2+ window.scrollY + "px"
+          contentWrapper.style.left = left +width+ window.scrollX + "px"
+        }
         },
       eventHandler(e) {
         if (this.$refs.contentWrapper && (this.$refs.contentWrapper.contains(e.target))||this.$refs.popover.contains(e.target)) {
@@ -75,24 +96,70 @@
   .content-wrapper {
     border: 1px solid $border-color;
     position: absolute;
-    transform: translateY(-100%);
     background-color: #ffffff;
     /*box-shadow: 0 0 3px rgba(0, 0, 0, 0.7);*/
-    filter: drop-shadow(0px 1px 2px rgba(0, 0, 0, 0.7));
+    filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.8));
     padding: 0.5em 1em;
     max-width: 20em;
     border-radius: $border-radius;
-    margin-top: -12px;
     &::after,&::before{
       content:'';
       width:0;
       height:0;
-      border:12px solid transparent;
+      border:10px solid transparent;
       position:absolute;
-      left:12px;
-      top:100%
+     }
+    &.position-top{
+     transform: translateY(-100%);
+      margin-top: -10px;
+      &::after,&::before{
+        left:10px;
+
+      }
+      &::after{border-top-color: #ffffff;
+        top:calc(100% - 2px);
+      }
+      &::before{border-top-color:#333333; top:100%}
     }
-    &::after{border-top-color: #ffffff;top:calc(100% - 2px);}
-    &::before{border-top-color:#333333}
+    &.position-bottom{
+      margin-top: 10px;
+      &::after,&::before{
+        left:10px;
+      }
+      &::after{border-bottom-color: #ffffff;bottom:calc(100% - 2px);}
+      &::before{border-bottom-color:#333333;bottom:100%;}
+
+    }
+    &.position-left{
+    transform: translateX(-100%);
+      margin-left: -10px;
+      &::after,&::before{
+        right:0;
+        top:6px;
+       transform: translateX(100%);
+      }
+      &::after{
+        border-left-color: #ffffff;
+        transform: translateX(calc(100% - 2px));
+
+      }
+      &::before{border-left-color:#333333;
+    }
+
+    }
+    &.position-right{
+margin-left: 10px;
+      &::after,&::before{
+       left:0;
+        top:6px;
+        transform: translateX(-100%);
+      }
+      &::after{
+        border-right-color: #ffffff;
+        transform: translateX(calc(-100% + 2px));
+      }
+      &::before{border-right-color:#333333;
+      }
+    }
   }
 </style>
