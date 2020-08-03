74bdbd7ca524ebdeeb9a4ac0f9f47584ffc94ef0
Administrator <yuy20170301@126.com>
2020-08-03 11:52:45 +0800

    collapse组件创建，完成基本样式

---
+++ src/Collapse.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="collapse">
+    <slot>
+
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Collapse"
+  }
+</script>
+
+<style lang="scss" scoped>
+  $border-color:grey;
+  $border-radius:4px;
+.collapse{
+border:1px solid $border-color;
+  border-radius:$border-radius ;
+
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ import TabsBody from './Tabs-body'
 import TabsItem from'./Tabs-item'
 import TabsPane from './Tabs-pane'
 import Popover from "./Popover";
+import Collapse from "./Collapse";
+import CollapseItem from './Collapse-item'
 Vue.component("g-input", Input)
 Vue.component("g-button", Button)
 Vue.component("icon", Icon)
@@ -37,14 +39,16 @@ Vue.component('g-tabs-body',TabsBody)
 Vue.component('g-tabs-pane',TabsPane)
 Vue.component('g-tabs-item',TabsItem)
 Vue.component('g-popover',Popover)
+Vue.component('g-collapse',Collapse)
+Vue.component('g-collapse-item',CollapseItem)
 new Vue({
   el: "#app",
   data: {
-    // loading1: false,
-    // loading2: false,
-    // loading3: false,
-    // message: "你好",
-    // selectedTab:'sports'
+    loading1: false,
+    loading2: false,
+    loading3: false,
+    message: "你好",
+    selectedTab:'sports'
 
   },
   methods: {
